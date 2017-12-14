7d37c61c5c71dc6a5793384562006d8dc9bf8db8
nathan moreau <nathan.moreau@m4x.org>
2017-12-15 00:02:25 +0100

    Extract reused code from day 10.

--- src/day10-knot-hash.rs
+++ src/day10-knot-hash.rs
@@ -34,61 +34,14 @@ fn solve(nb_elements: i32, input: &str) -> i32 {
     elts.iter().take(2).cloned().product()
 }
 
-fn xorall(it: &[i32]) -> Option<i32> {
-    use std::ops::BitXor;
-    let mut it = it.iter().cloned();
-    it.next().map(|st| it.fold(st, i32::bitxor))
-}
-
-fn hexa(n: i32) -> char {
-    match n {
-        0 => '0',
-        1 => '1',
-        2 => '2',
-        3 => '3',
-        4 => '4',
-        5 => '5',
-        6 => '6',
-        7 => '7',
-        8 => '8',
-        9 => '9',
-        10 => 'a',
-        11 => 'b',
-        12 => 'c',
-        13 => 'd',
-        14 => 'e',
-        15 => 'f',
-        _ => panic!("hexa"),
-    }
-}
-
-fn hexa2(n:i32) -> String {
-    vec![n / 16, n % 16].iter().cloned().map(hexa).collect()
-}
-
-fn hash(sparse_hash: Vec<i32>) -> String {
-    let chunks : Vec<_> = sparse_hash.chunks(16)
-        .map(|chunk| xorall(chunk).unwrap())
-        .map(hexa2)
-        .collect();
-    chunks.join("")
-}
-
-fn solve2(nb_elements: i32, input: &str) -> String {
-    let mut elts :Vec<_> = (0..nb_elements).collect();
-    let mut input : Vec<_> = input.trim().as_bytes().iter().cloned().map(|b| b as i32).collect();
-    input.append(&mut (vec![17, 31, 73, 47, 23]));
-    (0..64).fold(State {
-        cur_pos: 0,
-        skip_size: 0,
-    }, |st,_| permute(&mut elts, st, input.iter().cloned()));
-    hash(elts)
+fn solve2(input: &str) -> String {
+    String::from_utf8(lib::knot_hash(input.trim().as_bytes())).unwrap()
 }
 
 fn main() {
     let input = lib::read_input_file().unwrap();
     println!("{}", solve(256, &input));
-    println!("{}", solve2(256, &input));
+    println!("{}", solve2(&input));
 }
 
 #[cfg(test)]
@@ -98,12 +51,4 @@ mod tests {
     fn test_pb1() {
         assert_eq!(12, solve(5, "3,4,1,5\n\n"));
     }
-    #[test]
-    fn test_hash() {
-        assert_eq!("a2582a3a0e66e6e86e3812dcb672a272", solve2(256, ""));
-        assert_eq!("33efeb34ea91902bb2f59c9920caa6cd", solve2(256, "AoC 2017"));
-        assert_eq!("3efbe78a8d82f29979031a4aa0b16a9d", solve2(256, "1,2,3"));
-        assert_eq!("63960835bcdc130f0b66d7ff4f6a5a8e", solve2(256, "1,2,4"));
-    }
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,60 @@ pub fn parse_from<'a, T : Copy>(table: &'a [(&'static str, T)], input: &'a str) 
         .map(|&(_, value)| value)
         .expect(("parse_from: ".to_owned() + &input.to_owned()).as_str())
 }
+
+fn hexa2(n:i32) -> Vec<u8> {
+    vec![n / 16, n % 16].iter().cloned().map(hexa).collect()
+}
+
+fn sparse_to_dense(sparse_hash: Vec<i32>) -> Vec<u8> {
+    sparse_hash.chunks(16)
+        .map(|chunk| chunk.iter().cloned().fold(0, |acc, x| acc^x))
+        .flat_map(hexa2)
+        .collect()
+}
+
+fn hexa(n: i32) -> u8 {
+    match n {
+        0 => b'0', 1 => b'1', 2 => b'2', 3 => b'3',
+        4 => b'4', 5 => b'5', 6 => b'6', 7 => b'7',
+        8 => b'8', 9 => b'9', 10 => b'a', 11 => b'b',
+        12 => b'c', 13 => b'd', 14 => b'e', 15 => b'f',
+        _ => panic!("hexa"),
+    }
+}
+
+pub fn permute<Iter: Iterator<Item = i32>>(elts: &mut Vec<i32>, st: (i32,i32), input: Iter) -> (i32,i32) {
+    let nb_elements = elts.len() as i32;
+    input.fold(st, |(cur_pos, skip_size), len| {
+        assert!(0 <= cur_pos);
+        let mut lo = cur_pos;
+        let mut hi = cur_pos + len - 1;
+        while lo < hi {
+            elts.swap((lo % nb_elements) as usize, (hi % nb_elements) as usize );
+            lo += 1;
+            hi -= 1;
+        }
+        (cur_pos + len + skip_size as i32, skip_size + 1)
+    })
+}
+
+pub fn knot_hash(input: &[u8]) -> Vec<u8> {
+    let mut elts :Vec<_> = (0..256).collect();
+    let mut input : Vec<_> = input.iter().cloned().map(|b| b as i32).collect();
+    input.append(&mut (vec![17, 31, 73, 47, 23]));
+    (0..64).fold((0,0), |st,_| permute(&mut elts, st, input.iter().cloned()));
+    sparse_to_dense(elts)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_knot_hash() {
+        assert_eq!(b"a2582a3a0e66e6e86e3812dcb672a272", &knot_hash(b"")[..]);
+        assert_eq!(b"33efeb34ea91902bb2f59c9920caa6cd", &knot_hash(b"AoC 2017")[..]);
+        assert_eq!(b"3efbe78a8d82f29979031a4aa0b16a9d", &knot_hash(b"1,2,3")[..]);
+        assert_eq!(b"63960835bcdc130f0b66d7ff4f6a5a8e", &knot_hash(b"1,2,4")[..]);
+    }
+}
