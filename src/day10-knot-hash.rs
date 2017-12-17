39f22f6ac1469065ec5fd96a281d0e19785267f2
nathan moreau <nathan.moreau@m4x.org>
2017-12-17 01:50:13 +0100

    Deduplicate.

--- src/day10-knot-hash.rs
+++ src/day10-knot-hash.rs
@@ -1,36 +1,9 @@
 mod lib;
 
-struct State {
-    cur_pos: i32,
-    skip_size: i32,
-}
-
-fn permute<Iter: Iterator<Item = i32>>(elts: &mut Vec<i32>, st: State, input: Iter) -> State {
-    let nb_elements = elts.len() as i32;
-    input.fold(st, |State {cur_pos, skip_size}, len| {
-        assert!(0 <= cur_pos);
-        let mut lo = cur_pos;
-        let mut hi = cur_pos + len - 1;
-        while lo < hi {
-            elts.swap((lo % nb_elements) as usize, (hi % nb_elements) as usize );
-            lo += 1;
-            hi -= 1;
-        }
-        State {
-            cur_pos: cur_pos + len + skip_size as i32,
-            skip_size: skip_size + 1
-        }
-    })
-}
-
 fn solve(nb_elements: i32, input: &str) -> i32 {
     let input = input.trim().split(",").map(lib::parse_i32);
     let mut elts :Vec<_> = (0..nb_elements).collect();
-    let st = State {
-        cur_pos: 0,
-        skip_size: 0,
-    };
-    permute(&mut elts, st, input);
+    lib::permute(&mut elts, (0,0), input);
     elts.iter().take(2).cloned().product()
 }
 
